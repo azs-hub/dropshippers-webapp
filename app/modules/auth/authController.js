ed298d300e69ee5562c9909a62c473a143fcf2b5
Antoine SCOFFIER <ascoffier@octo.com>
2016-07-31 16:25:51 +0200

    Authent is ok

--- app/modules/auth/authController.js
+++ app/modules/auth/authController.js
@@ -2,11 +2,26 @@
 
 angular.module('wecommerceApp')
   .controller('AuthController',
-    ['$scope', '$state',
-      function ($scope, $state) {
+    ['$scope', '$state', '$auth', 'AuthService',
+      function ($scope, $state, $auth, AuthService) {
+        $scope.user = null;
         $scope.login = function () {
-          $state.go('dashboard');
+          if ($scope.user != null) 
+          {
+            AuthService.login($scope.user)
+              .then(function(response) {
+                if (response.status == 200)
+                {
+                  console.log("then",response);
+                  $auth.setToken(response);
+                }
+                console.log("status not 200");
+                
+              });
+          }
+          
         };
+
         $scope.Password = 'password';
       }
     ]
--- app/modules/auth/authService.js
+++ app/modules/auth/authService.js
@@ -21,6 +21,23 @@ angular.module('wecommerceApp')
               console.log("errorCallback : ", response);
               return response;
             });
+        },
+        login: function(datas) {
+          $log.debug('Auth:login', datas);
+          return $http({
+            method: 'POST',
+            url: BASE_URL_API + 'login/signin',
+            params: {
+              username: datas.username,
+              password: datas.password
+            }
+          }).then(function successCallback(response) {
+              console.log("successCallback : ", response);
+              return response;
+            }, function errorCallback(response) {
+              console.log("errorCallback : ", response);
+              return response;
+            });
         }
       };
     }
