511564552170cc5dca1939125d73a38e61a3cf90
Antoine SCOFFIER <ascoffier@octo.com>
2016-07-31 18:13:56 +0200

    Get product is OK, we can take products from database

--- app/app.js
+++ app/app.js
@@ -9,6 +9,41 @@ angular.module('wecommerceApp', [
     'ngAria',
     'ngLodash',
     'satellizer'])
+    .factory('SatellizerInterceptor', [
+      '$q',
+      'SatellizerConfig',
+      'SatellizerStorage',
+      'SatellizerShared',
+      function($q, config, storage, shared) {
+        return {
+          request: function(request) {
+            if (request.skipAuthorization) {
+              return request;
+            }
+
+            console.log('---->', config, shared, storage.get("token"));
+
+            if (shared.isAuthenticated() && config.httpInterceptor(request)) {
+              //var tokenName = config.tokenPrefix ? config.tokenPrefix + '_' + config.tokenName : config.tokenName;
+              //var token = storage.get("token");
+
+              //if (config.authHeader && config.authToken) {
+                //token = storage.get("satellizer_token");
+              //}
+
+              request.headers["token"] = storage.get("satellizer_token");
+            }
+
+            return request;
+          },
+          responseError: function(response) {
+            return $q.reject(response);
+          }
+        };
+      }])
+    .config(['$httpProvider', function($httpProvider) {
+      $httpProvider.interceptors.push('SatellizerInterceptor');
+    }])
     .config( ['$stateProvider', '$urlRouterProvider', '$authProvider', 'BASE_URL_API',
         function($stateProvider, $urlRouterProvider, $authProvider, BASE_URL_API) {
             $urlRouterProvider.otherwise('/');
@@ -16,8 +51,9 @@ angular.module('wecommerceApp', [
             $authProvider.baseUrl = BASE_URL_API;
             $authProvider.loginUrl = "login/signin";
             $authProvider.tokenName = "token";
-            $authProvider.authHeader = "auth-token";
-
+            $authProvider.authHeader = "token";
+            $authProvider.tokenHeader = "token";
+            
             $stateProvider
               .state('login', {
                 url: '/login',
--- app/modules/home/homeController.js
+++ app/modules/home/homeController.js
@@ -2,9 +2,18 @@
 
 angular.module('wecommerceApp')
   .controller('HomeController',
-    ['$scope', '$state',
-      function ($scope, $state) {
-        console.log('toto')
+    ['$scope', '$state', '$auth', 'ProductService', 
+      function ($scope, $state, $auth, ProductService) {
+      	$scope.products = {};
+      	$scope.user = {
+      		isAuth: $auth.isAuthenticated()
+      	};
+
+        if ($auth.isAuthenticated) {
+        	ProductService.getProducts().then(function(res) {
+        		$scope.products = res;
+        	});
+	    }
       }
     ]
   );
