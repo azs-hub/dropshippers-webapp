f0401870895f552413725df8e69edc41c9e82a71
Anais Siba <anais.siba@gmail.com>
2016-07-31 20:29:48 +0200

    Nouvelle archi - gulp stable

--- app/modules/profile/profile.module.js
+++
@@ -1,5 +0,0 @@
-angular.module('ds.profile', [
-  'profile.service',
-  'auth.controller',
-  'signin.controller'
-]);
--- app/scripts/config.js
+++
@@ -1,3 +0,0 @@
-angular.module("local.config", [])
-
-.constant("BASE_URL_API", "http://dropshippers.dev/v1/");
