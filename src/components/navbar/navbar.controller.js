d743f3e16b7ef84b5942dc16e724e727faf49659
Anais Siba <anais.siba@gmail.com>
2016-09-03 17:54:34 +0200

    login page

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,6 +1,13 @@
 angular.module('navbar.controller', [])
-    .controller("NavbarController", ['$scope',
-        function ($scope) {
+    .controller("NavbarController", ['$scope', '$state',
+        function ($scope, $state) {
             
+            console.log($state.$current.toString(), $state.current);
+        	$scope.$state = $state;
 
+        	$scope.showLogo = function (stateName) {
+        		if (stateName != "login" && stateName != "signin")
+        			return true;
+        		return false;
+        	}
         }]);
