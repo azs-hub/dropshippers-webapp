4b0b7e8341e87f69e661c0c7d025c991292f9114
Anais Siba <anais.siba@gmail.com>
2016-09-08 17:30:56 +0200

    all propositions

--- src/app/propositions/propositionsController.js
+++ src/app/propositions/propositionsController.js
@@ -2,8 +2,8 @@
 
 angular.module('dropshippers')
   .controller('PropositionsController',
-    ['$scope', '$state', 'PropositionService', 'NgTableParams',
-      function ($scope, $state, PropositionService, NgTableParams) {
+    ['$scope', '$state', '$filter', 'PropositionService', 'NgTableParams',
+      function ($scope, $state, $filter, PropositionService, NgTableParams) {
 
       	$scope.tableParams = new NgTableParams({
           count: 10,
@@ -18,15 +18,22 @@ angular.module('dropshippers')
               if (res.status != 200)
                 return null;
               else {
-              	console.log('res all ', res);
-                // var filteredData = params.filter() ?
-                //       $filter('filter')(res.data.propositions[product.product.dropshippers_ref],  params.filter()) :
-                //       res.data.propositions[product.product.dropshippers_ref];
-                // var orderedData = params.sorting() ?
-                //     $filter('orderBy')(filteredData, params.orderBy()) :
-                //     filteredData;
-                // params.total(orderedData.length);
-                // return orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+              	
+              	var data = [];
+
+              	for (var i in res.data.propositions) {
+              		_.sortBy(res.data.propositions[i], function(o) { return o.updated_at; })
+              		data.push(res.data.propositions[i][0]);
+              	}
+
+                var filteredData = params.filter() ?
+                      $filter('filter')(data,  params.filter()) :
+                      data;
+                var orderedData = params.sorting() ?
+                    $filter('orderBy')(filteredData, params.orderBy()) :
+                    filteredData;
+                params.total(orderedData.length);
+                return orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
               }
             });
           },
