4b83a5d6c0c17db92513735af4bf3090fe2fa33f
root <root@dropshippers.io>
2016-10-15 13:59:13 -0400

    clean propositionsCtrl merge

--- src/app/propositions/propositionsController.js
+++ src/app/propositions/propositionsController.js
@@ -27,22 +27,7 @@ angular.module('dropshippers')
             params.total(orderedData.length);
             return orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
 
-<<<<<<< HEAD
-              if (res.status != 200)
-                return null;
-              else {
-                var filteredData = params.filter() ?
-                      $filter('filter')(res.data.propositions,  params.filter()) :
-                      res.data.propositions;
 
-                var orderedData = params.sorting() ?
-                    $filter('orderBy')(filteredData, params.orderBy()) :
-                    filteredData;
-                params.total(orderedData.length);
-                return orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-              }
-            });
-=======
                 // var filteredData = params.filter() ?
                 //       $filter('filter')(res.data.propositions,  params.filter()) :
                 //       res.data.propositions;
@@ -53,7 +38,7 @@ angular.module('dropshippers')
                 // return orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
             //   }
             // });
->>>>>>> 28aea2d80fd65a50689df65363970f352af40786
+
           },
         paginationMaxBlocks: 5,
         paginationMinBlocks: 2
